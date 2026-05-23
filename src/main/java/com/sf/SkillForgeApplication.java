52c6c53cd041f089c52246757f731658adfa9665
rahulrathod6203 <rahulrathod6203@gmail.com>
2026-05-23 13:17:46 +0530

    chore : mysql database properties added using application.yml file and added external database configuration using dotenv file

--- src/main/java/com/sf/SkillForgeApplication.java
+++ src/main/java/com/sf/SkillForgeApplication.java
@@ -1,13 +1,22 @@
 package com.sf;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkillForgeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkillForgeApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
+
+        SpringApplication.run(SkillForgeApplication.class, args);
+    }
 
 }
