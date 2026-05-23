004c6f3b3f97db1a13d34c6b0af1f41f22392f47
rahulrathod6203 <rahulrathod6203@gmail.com>
2026-05-23 15:55:22 +0530

    chore : controller layer implementation added

--- src/main/java/com/sf/appUser/controller/AppUserController.java
+++ src/main/java/com/sf/appUser/controller/AppUserController.java
@@ -3,42 +3,58 @@ package com.sf.appUser.controller;
 import com.sf.appUser.dto.AppUserRequest;
 import com.sf.appUser.dto.AppUserResponse;
 import com.sf.appUser.service.AppUserService;
-import jakarta.persistence.PostUpdate;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/appUsers")
 @RequiredArgsConstructor
+@Slf4j
 public class AppUserController {
 
     private final AppUserService appUserService;
 
     @PostMapping
-    public AppUserResponse createUser(AppUserRequest request) {
-        return null;
+    public ResponseEntity<AppUserResponse> createUser(@Valid @RequestBody AppUserRequest request) {
+
+        log.info("AppUser is being registered...");
+        AppUserResponse createdUser = appUserService.createUser(request);
+        log.info("AppUser registered... {}", createdUser);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdUser.id()).toUri();
+
+        return ResponseEntity.created(location).body(createdUser);
     }
 
     @GetMapping
-    public List<AppUserResponse> getAllUsers() {
-        return List.of();
+    public ResponseEntity<List<AppUserResponse>> getAllUsers() {
+        log.info("Fetching all users...");
+        return ResponseEntity.ok(appUserService.getAllUsers());
     }
 
 
     @GetMapping("/{id}")
-    public AppUserResponse getUserById(Long id) {
-        return null;
+    public ResponseEntity<AppUserResponse> getUserById(Long id) {
+        log.info("Fetching single user by id...");
+        return ResponseEntity.ok(appUserService.getUserById(id));
     }
 
     @PutMapping("/{id}")
-    public AppUserResponse updateUser(Long id, AppUserRequest request) {
-        return null;
+    public ResponseEntity<AppUserResponse> updateUser(Long id, AppUserRequest request) {
+        log.info("Updating existing single user by id...");
+        return ResponseEntity.accepted().body(appUserService.updateUser(id, request));
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUserById(Long id) {
-
+    public ResponseEntity<Object> deleteUserById(Long id) {
+        log.info("Deleting existing single user by id...");
+        appUserService.deleteUserById(id);
+        return ResponseEntity.noContent().build();
     }
 }
